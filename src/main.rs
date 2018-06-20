c3479250d8e1e9de40378eac8e4d37d1f9b1c76e
Psilon <pzixel@gmail.com>
2018-06-20 23:56:22 +0300

    foo

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,17 @@
-extern crate bodyparser;
-extern crate config;
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate iron;
-extern crate itertools;
 extern crate log4rs;
-#[macro_use]
-extern crate log;
-extern crate serde;
+
 #[macro_use]
 extern crate serde_derive;
+extern crate serde;
 extern crate serde_json;
-extern crate sha2;
-extern crate tokio_core;
 
 mod contract;
 mod telegram_client;
 
-use contract::*;
-use iron::prelude::*;
-use itertools::Itertools;
-use sha2::{Digest, Sha256};
-use std::borrow::{Borrow, Cow};
-use std::env;
-use std::error::Error as ErrorTrait;
-use std::fmt;
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
-use telegram_client::*;
-
 const STORAGE_DIR_NAME: &str = "storage";
 
-#[derive(Debug, Clone, Deserialize)]
-struct Settings {
-    address: String,
-}
-
 fn main() {
-    log4rs::init_file("log4rs.toml", Default::default()).unwrap();
-    let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("Settings")).unwrap();
-    let settings = settings.try_into::<Settings>().unwrap();
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!(
-            "Expected bot token as single parameter, but found {} parameters!",
-            args.len() - 1
-        );
-    }
-    std::fs::create_dir_all(STORAGE_DIR_NAME).unwrap();
-    let bot_token = args[1].clone();
-
-    debug!("Starting application with bot token {}", bot_token);
-    let chain = Chain::new(move |r: &mut Request| web_hook(r, &bot_token));
-    Iron::new(chain).http(settings.address).unwrap();
-}
-
-fn web_hook(request: &mut Request, bot_token: &String) -> IronResult<Response> {
-    match core(request, bot_token) {
-        Ok(_) => Ok(Response::with((iron::status::Ok))),
-        Err(error) => {
-            error!("Error while processing the request: {:?}", error);
-            let message = String::from(error.description());
-            Err(IronError {
-                error,
-                response: Response::with((iron::status::InternalServerError, message)),
-            })
-        }
-    }
-}
-
-fn core(request: &mut Request, bot_token: &String) -> Result<(), Box<ErrorTrait + Send + Sync>> {
-    let update = request.get::<bodyparser::Struct<Update>>()?;
-    match update {
-        Some(update) => {
-            let update: Update = update;
-            let chat_id = update.message.chat.id;
-            let mut client = TelegramClient::new(bot_token);
-            client.send_message(chat_id, "Hello from bot!")?;
-
-            if let Some(document) = update.message.document {
-                handle_document(&mut client, &document.file_id)?;
-            }
-            if let Some(photos) = update.message.photo {
-                debug!("Found some photos! {} items", photos.len());
-                for photo in photos {
-                    handle_document(&mut client, &photo.file_id)?;
-                }
-            }
-
-            Ok(())
-        }
-        _ => {
-            const COULD_NOT_PARSE_UPDATE_MESSAGE: &str = "Could not parse update object";
-            Err(Box::new(BotError::new(COULD_NOT_PARSE_UPDATE_MESSAGE)).into())
-        }
-    }
-}
-
-fn handle_document(client: &mut TelegramClient, file_id: &str) -> Result<(), Box<ErrorTrait + Send + Sync>> {
-    let file = client.get_file(file_id)?;
-    match file.file_path {
-        Some(file_path) => {
-            let file_bytes = client.download_file(&file_path)?;
-            let mut hasher = Sha256::default();
-            hasher.input(&file_bytes);
-            let output = hasher.result();
-            let file_hash = format!("{:02x}", output.iter().format(""));
-            let filename = Path::new(&file_hash);
-            let filename = match Path::new(&file_path).extension() {
-                Some(extension) => filename.with_extension(extension),
-                _ => filename.to_path_buf(),
-            };
-            debug!("Processing file: {}. Resulting path: {:?}", file_path, filename);
-            let mut file = File::create(Path::new(STORAGE_DIR_NAME).join(filename))?;
-            file.write_all(&file_bytes)?;
-            Ok(())
-        }
-        _ => Err(Box::new(BotError::new("File contains no path!")).into()),
-    }
-}
-
-#[derive(Debug)]
-struct BotError<'a> {
-    details: Cow<'a, str>,
-}
-
-impl<'a> BotError<'a> {
-    fn new<S: Into<Cow<'a, str>>>(msg: S) -> BotError<'a> {
-        BotError { details: msg.into() }
-    }
-}
-
-impl<'a> fmt::Display for BotError<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.details)
-    }
-}
-
-impl<'a> ErrorTrait for BotError<'a> {
-    fn description(&self) -> &str {
-        self.details.borrow()
-    }
 }
--- src/telegram_client.rs
+++ src/telegram_client.rs
@@ -6,34 +6,31 @@ use hyper;
 use hyper::client::HttpConnector;
 use hyper::{Client, Method, Request};
 use hyper_tls::HttpsConnector;
-use serde;
-use serde_json;
 use std;
 use std::io;
-use tokio_core::reactor::Core;
+use serde::export;
+use serde_json::from_slice;
+use hyper::Response;
 
 pub struct TelegramClient<'a> {
     token: &'a String,
     client: Client<HttpsConnector<HttpConnector>, hyper::Body>,
-    core: Core,
 }
 
 impl<'a> TelegramClient<'a> {
-    pub fn new(token: &'a String) -> Self {
-        let core = Core::new().unwrap();
-        let handle = core.handle();
+    pub fn new(token: &'a String, handle: &()) -> Self {
         let client = Client::configure()
-            .connector(HttpsConnector::new(4, &handle).unwrap())
+            .connector(HttpsConnector::new(4).unwrap())
             .build(&handle);
-        TelegramClient { token, client, core }
+        TelegramClient { token, client, }
     }
 
-    pub fn send_message(&mut self, chat_id: i64, text: &str) -> serde::export::Result<hyper::Response, hyper::Error> {
+    pub fn send_message(&mut self, chat_id: i64, text: &str) -> export::Result<Response<()>, hyper::Error> {
         let url = format!("bot{}/sendMessage?chat_id={}&text={}", self.token, chat_id, text);
         self.send(Method::Post, &url)
     }
 
-    pub fn get_file(&mut self, file_id: &str) -> serde::export::Result<File, hyper::Error> {
+    pub fn get_file(&mut self, file_id: &str) -> export::Result<File, hyper::Error> {
         let url = format!("bot{}/getFile?file_id={}", self.token, file_id);
         self.send_then(Method::Get, &url, |res| {
             res.body().concat2().and_then(move |body| {
@@ -41,13 +38,13 @@ impl<'a> TelegramClient<'a> {
                 let response = String::from_utf8_lossy(&v).to_string();
                 println!("Response = {}", response);
                 let v: GetFileResponse =
-                    serde_json::from_slice(&body).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                    from_slice(&body).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
                 Ok(v.result)
             })
         })
     }
 
-    pub fn download_file(&mut self, file_path: &str) -> serde::export::Result<Vec<u8>, hyper::Error> {
+    pub fn download_file(&mut self, file_path: &str) -> export::Result<Vec<u8>, hyper::Error> {
         let url = format!("file/bot{}/{}", self.token, file_path);
         self.send_then(Method::Get, &url, |res| {
             res.body().concat2().and_then(move |body| {
@@ -57,22 +54,22 @@ impl<'a> TelegramClient<'a> {
         })
     }
 
-    fn send(&mut self, method: Method, url: &str) -> serde::export::Result<hyper::Response, hyper::Error> {
+    fn send<T>(&mut self, method: Method, url: &str) -> export::Result<Response<T>, hyper::Error> {
         self.send_then(method, url, |x| Ok(x))
     }
 
-    fn send_then<F, B>(
+    fn send_then<T, F, B>(
         &mut self,
         method: Method,
         url: &str,
         f: F,
-    ) -> serde::export::Result<<B as futures::IntoFuture>::Item, hyper::Error>
+    ) -> export::Result<<B as futures::IntoFuture>::Item, hyper::Error>
     where
-        F: std::ops::FnOnce(hyper::Response) -> B,
+        F: std::ops::FnOnce(Response<T>) -> B,
         B: IntoFuture<Error = hyper::Error>,
     {
         let uri = format!("https://api.telegram.org/{}", url).parse().unwrap();
         let request = self.client.request(Request::new(method, uri)).and_then(f);
-        self.core.run(request)
+        request
     }
 }
