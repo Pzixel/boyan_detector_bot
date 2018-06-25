efe7c04dc512de9a0b063551da234690513f8d8c
Psilon <pzixel@gmail.com>
2018-06-25 23:04:25 +0300

    works

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ use clap::{App, Arg};
 use contract::{File, Update};
 use futures::future;
 use futures::future::Either;
+use futures::IntoFuture;
 use futures::Stream;
 use hyper::rt::{self, Future};
 use hyper::service::service_fn;
@@ -112,10 +113,14 @@ fn echo(
     req: Request<Body>,
     telegram_client: Arc<TelegramClient>,
 ) -> impl Future<Item = Response<Body>, Error = hyper::Error> + Send {
-    let result = req.into_body().concat2().and_then(move |chunk| {
-        let result = from_slice::<Update>(chunk.as_ref());
-        match result {
-            Ok(update) => {
+    req.into_body().concat2().and_then(move |chunk| {
+        from_slice::<Update>(chunk.as_ref())
+            .into_future()
+            .map_err(|_| Response::builder()
+                .status(StatusCode::UNPROCESSABLE_ENTITY)
+                .body(Body::empty())
+                .expect("known safe response parameters"))
+            .and_then(|update| {
                 let chat_id = update.message.chat.id;
                 let message_id = update.message.message_id;
                 let file_id = match (&update.message.document, &update.message.photo) {
@@ -126,60 +131,46 @@ fn echo(
                         .map(|x| &x.file_id),
                     _ => None,
                 };
+                file_id
+                    .map(|f| (f.clone(), chat_id, message_id))
+                    .ok_or_else(|| Response::new(Body::empty()))
+            })
+            .and_then(move |(file_id, chat_id, message_id)| {
+                telegram_client.get_file(&file_id)
+                    .and_then(move |file| {
+                        info!("Checking file {:?}", file);
 
-                match file_id {
-                    Some(file_id) => Either::A({
-                        let f = telegram_client.get_file(file_id).and_then(move |file| {
-                            info!("Checking file {:?}", file);
+                        let file_path = file.file_path.clone();
 
-                            let file_path = file.file_path.clone();
-
-                            let inner_f = if let Some(file_path) = get_file_path_if_processable(file_path) {
-                                Either::A(telegram_client.download_file(&file_path).and_then(move |bytes| {
-                                    telegram_client.send_message(
-                                        chat_id,
-                                        &format!(
-                                            "Hello from bot. Got file with id: {}. File length is {} bytes",
-                                            file.file_id,
-                                            bytes.len()
-                                        ),
-                                        Some(message_id),
-                                    )
-                                }))
-                            } else {
-                                info!("Skipping unsupported extension");
-                                Either::B(future::ok(()))
-                            };
-                            inner_f
-                        });
-                        f.then(move |result| {
-                            let result = match result {
-                                Ok(_) => Either::A(future::ok(Response::new(Body::empty()))),
-                                Err(e) => {
-                                    error!("Error while processing: {}", e);
-                                    Either::B(future::ok(
-                                        Response::builder()
-                                            .status(StatusCode::INTERNAL_SERVER_ERROR)
-                                            .body(Body::empty())
-                                            .unwrap(),
-                                    ))
-                                }
-                            };
-                            result
-                        })
-                    }),
-                    None => Either::B(future::ok(Response::new(Body::empty()))),
-                }
-            }
-            Err(_) => Either::B(future::ok(
-                Response::builder()
-                    .status(StatusCode::UNPROCESSABLE_ENTITY)
-                    .body(Body::empty())
-                    .unwrap(),
-            )),
-        }
-    });
-    result
+                        if let Some(file_path) = get_file_path_if_processable(file_path) {
+                            Either::A(telegram_client.download_file(&file_path).and_then(move |bytes| {
+                                telegram_client.send_message(
+                                    chat_id,
+                                    &format!(
+                                        "Hello from bot. Got file with id: {}. File length is {} bytes",
+                                        file.file_id,
+                                        bytes.len()
+                                    ),
+                                    Some(message_id),
+                                )
+                            }))
+                        } else {
+                            info!("Skipping unsupported extension");
+                            Either::B(future::ok(()))
+                        }
+                    })
+                    .map_err(|e| {
+                        error!("Error while processing: {}", e);
+                        Response::builder()
+                            .status(StatusCode::GATEWAY_TIMEOUT)
+                            .body(Body::empty())
+                            .expect("known safe response parameters")
+                    })
+            })
+            // ...and here we unify both paths
+            .map(|_| Response::new(Body::empty()))
+            .or_else(Ok)
+    })
 }
 
 fn get_file_path_if_processable(file_path: Option<String>) -> Option<String> {
